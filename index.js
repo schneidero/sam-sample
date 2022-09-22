215a17cea5fa52a3f6a8fc94ae397d114a1202ae
Neba Schneider Kintalen <desneirdero@gmail.com>
2022-09-22 09:57:54 +0100

    upload to s3

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const aws = require("aws-sdk");
-const s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
 exports.handler = async function (event, context) {
+  const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
   let name = process.env.PERSON_NAME;
   let requestId = context.awsRequestId;
-  console.log("Hello, " + name + "!");
-  console.log("RequestId, " + requestId + "!");
-  return context.logStreamName;
+  var fileContents = `${requestId} :: ${name}`;
+
+  var params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: `/lambda-logs/${requestId}.txt`,
+    Body: fileContents,
+    ContentType: "text/plain",
+  };
+
+  try {
+    let s3Response = await S3.upload(params).promise();
+
+    let res = {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        s3Path: s3Response.Location,
+      }),
+    };
+    console.log("Response", s3Response);
+    console.log("RequestId, " + requestId + "!");
+    return res;
+  } catch (error) {
+    let fail = {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        error: error,
+      }),
+    };
+
+    return fail;
+  }
 };
