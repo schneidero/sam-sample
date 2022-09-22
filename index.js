a99c6f8077ccdc4f35b0b4efceb18a5760643af6
Neba Schneider Kintalen <desneirdero@gmail.com>
2022-09-22 09:17:50 +0100

    added mem size

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const aws = require("aws-sdk");
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
 exports.handler = async function (event, context) {
   let name = process.env.PERSON_NAME;
+  let requestId = context.awsRequestId;
   console.log("Hello, " + name + "!");
+  console.log("RequestId, " + requestId + "!");
   return context.logStreamName;
 };
